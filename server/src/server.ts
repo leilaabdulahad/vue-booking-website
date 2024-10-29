27c7c182f63cae50a573964a92e2f024f9912d7a
Leila <leilaabdulahad@hotmail.com>
2024-10-29 13:34:29 +0100

    changed route file for consistency

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import propertyRoutes from '../routes/properties'
 import userRoutes from '../routes/users'
-import bookingRoutes from '../routes/bookingRoutes'
+import bookings from '../routes/bookings'
 import favoritesRoutes from '../routes/favorites'
 
 
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 app.use('/api/users', userRoutes)
-app.use('/api/bookings', bookingRoutes)
+app.use('/api/bookings', bookings)
 app.use('/api/favorites', favoritesRoutes)
 
 const MONGODB_URI = process.env.MONGODB_URI
